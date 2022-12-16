0d14366a04e87ce902d388c1d06d18c4497d1d56
acavih <registrosacavih@gmial.com>
2022-12-16 10:59:44 +0100

    Modificar eslint

---
+++ registrosfrontnuxt/store/auth.js
@@ -0,0 +1,24 @@
+export const state = () => ({
+  token: '',
+  userInfo: ''
+})
+
+/**
+* @type {import('vuex').GetterTree<ReturnType<typeof state>>, any>}
+*/
+export const getters = {
+}
+
+/**
+* @type {import('vuex').MutationTree<ReturnType<typeof state>>>}
+*/
+export const mutations = {
+}
+
+/**
+* @type {import('vuex').ActionTree<ReturnType<typeof state>>, any>}
+*/
+export const actions = {
+}
+
+export const strict = false
